ae3e79689d44bfd6071e864c18d22d95c88a7c17
AndrewJ1993 <andrew.s.jarrett1@gmail.com>
2020-03-26 15:11:52 +1100

    third commit

--- code.ino
+++ code.ino
@@ -1,21 +1,21 @@
 #define LIGHT_PIN A0 // Replace A0 with your pin 
-#define LIGHT_LEVEL_THRESHOLD 800 // Replace with your own light level 
-#define THRESHOLD_SENSITIVITY 50 // higher number = lower sensitivity, lower number = higher sensitivity 
+#define LIGHT_LEVEL_HIGH_THRESHOLD 800 // Replace with your own light level 
+#define LIGHT_LEVEL_LOW_THRESHOLD 200
 void setup() {
 } 
 
 // The light level is simulated by incrementing the light level every two seconds until it reaches the threshold. After which, an email is sent and the light will progressively decrement 
-// every 5 seconds until it is below the threshold, then another email is sent. The max_light_reached boolean is used to prevent the device sending an email every iteration of the loop
+// every 5 seconds until it is below the low threshold, then another email is sent. The max_light_reached boolean is used to prevent the device sending an email every iteration of the loop
 // before it ever reached the maximum light threshold.
 void loop() { 
     int light_level = 700;
     bool max_light_reached = false;
     while (true) {
-        if (light_level > LIGHT_LEVEL_THRESHOLD) { 
+        if (light_level > LIGHT_LEVEL_HIGH_THRESHOLD) { 
             Particle.publish("light-level-changed", "bright"); 
             max_light_reached = true;
-            // Once dark is triggered, the threshold is temporarily shifted so that the state will not float between dark and bright when it's on the threshold. This is how street lights work.
-            while (light_level > LIGHT_LEVEL_THRESHOLD - THRESHOLD_SENSITIVITY) {
+            
+            while (light_level > LIGHT_LEVEL_LOW_THRESHOLD) {
                 light_level -= 1;
                 delay(5000);
             } 
